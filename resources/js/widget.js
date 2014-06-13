6ff1e26213f275fe9a5f54ed343836bc3cb63e1a
Tal Giat <tal.giat@thomsonreuters.com>
2014-06-13 13:01:04 -0400

    commiting before presentation

--- resources/js/widget.js
+++ resources/js/widget.js
@@ -1,57 +1,128 @@
 $(function() {
-  React.renderComponent(
-    WCup.Components.GoalKeeper.Widget({}), 
-    document.getElementById('goalkeeper'));
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '526580450786887',
+      cookie     : true,  // enable cookies to allow the server to access the session
+      xfbml      : true,  // parse social plugins on this page
+      version    : 'v2.0' // use version 2.0
+    });
+    FB.getLoginStatus(function(response) {
+      WCup.facebook.status_change_callback(response);
+    });
+  };
+  // load facebook api
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 });
-function statusChangeCallback(response) {
-  if (response.status === 'connected') {
-    $('.fb_iframe_widget').hide();
-    testAPI();
-  } else {
-    $('#status').text('Please log into this app.');
+
+WCup.API = {
+  base_url: 'http://local.brazil2014.reutersmedia.net:30000',
+  update_user_info: function(user_info) {
+    var user_info = _.extend({
+      country: '',
+      player: '',
+      level: 0
+    }, user_info);
+    console.log(user_info);
+    return $.post(this.base_url + '/goalkeeper/users/' + user_info.id, user_info)
+      .fail(_.bind(function(xhr, status, err) {
+        console.error(xhr, status, err.toString());
+      }, this))
+      .done(_.bind(function(resp) {
+      }, this));
+  },
+  get_current_time: function() {
+    return moment().format('YYYY-MM-DD HH:mm:ss')+'-0400';
+  },
+  get_tommorow_time: function() {
+    return moment().add('h', 26).format('YYYY-MM-DD HH:mm:ss')+'-0400';
+  },
+  get_top_predictors: function(limit) {
+    var from = this.get_current_time();
+    var to = this.get_tommorow_time();
+    return $.getJSON(this.base_url + '/goalkeeper/users?from='+from+'&to='+to+'&limit=' + limit)
+      .fail(_.bind(function(xhr, status, err) {
+        console.error(xhr, status, err.toString());
+      }, this));
+  },
+  get_my_info: function(user_info) {
+    return $.getJSON(this.base_url + '/goalkeeper/users/?from='+from+'&to='+to+'&limit=' + limit)
   }
-}
-
-// This function is called when someone finishes with the Login
-// Button.  See the onlogin handler attached to it in the sample
-// code below.
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
-
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '526580450786887',
-    cookie     : true,  // enable cookies to allow the server to access the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.0' // use version 2.0
-  });
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
 };
 
-// Here we run a very simple test of the Graph API after login is
-// successful.  See statusChangeCallback() for when this call is made.
-function testAPI() {
-  FB.api('/me', function(response) {
-    console.log(response);
-    $('#status').text('Thanks for logging in, ' + response.name + '!');
-  });
-  FB.api("/me/picture", function (response) {
-    if (response && !response.error) {
-      console.log(response);
+WCup.facebook = {
+  get_user_info: function(callback) {
+    var user_info = null;
+    var picture_url = null;
+    var user_results = _.after(2, function() {
+      if (user_info && picture_url) {
+        callback(_.extend(user_info, { picture_url: picture_url}));
+      } else {
+        callback(null);
+      }
+    });
+    FB.api('/me', function(response) {
+      if (response && !response.error) {
+        user_info = {
+          first: response.first_name,
+          last: response.last_name,
+          id: response.id
+        };
+        user_results();
+      } else {
+        console.error(response);
+        user_results();
+      }
+    });
+    FB.api("/me/picture", function (response) {
+      if (response && !response.error) {
+        picture_url = response.data.url;
+        user_results();
+      } else {
+        console.error(response);
+        user_results();
+      }
+    });
+  },
+  status_change_callback: function (response) {
+    var create_widget = function(user) {
+      WCup.API.get_top_predictors(10).done(function(predictors) {
+        var Widget = WCup.Components.GoalKeeper.Widget;
+        if (user) {
+          var my_predictions;
+          _.each(predictors, function(predictor, index) {
+            if(predictor.id === parseInt(user.id)) {
+              my_predictions = _.extend({ rank: index + 1}, predictor);
+            }
+          });
+        }
+        React.renderComponent(
+          Widget({
+            users: _.first(predictors,5), 
+            user: user,
+            my_predictions: my_predictions
+          }), 
+          document.getElementById('goalkeeper')
+        );
+      });
+    };
+    if (response.status === 'connected') {
+      this.get_user_info(function(user_info) {
+        WCup.API.update_user_info(user_info);
+        create_widget(user_info);
+      });
+    } else {
+      create_widget();
     }
-  });
-}
-
-// Load the SDK asynchronously
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+  },
+  check_login_state: function() {
+    FB.getLoginStatus(_.bind(function(response) {
+      this.status_change_callback(response);
+    },this));
+  }
+};
--- src/jsx/goalkeeper.js
+++ src/jsx/goalkeeper.js
@@ -7,54 +7,125 @@ WCup.Components.GoalKeeper = _.extend(WCup.Components.GoalKeeper || {}, (functio
   return {
     Row: React.createClass({
       render: function() {
+        var get_flag = function(game) {
+          if (game && game.prediction !== null) {
+            var flags = {
+              0: game.team0,
+              1: game.team1,
+              2: 'TIE'
+            };
+            return flags[game.prediction] || 'EMPTY'; 
+          } else {
+            return 'EMPTY';
+          }
+        };
+        var score = this.props.user.score ? 
+          Math.floor(this.props.user.score * 1.0 / this.props.user.games * 100) + '%' : 
+          '--';
+        var level = {
+          0: 'novice',
+          1: 'intermediate',
+          2: 'expert'
+        };
+        var flag0 = 'flag-small flag0 ' + get_flag(this.props.user.predictions[0]);
+        var flag1 = 'flag-small flag1 ' + get_flag(this.props.user.predictions[1]);
+        var flag2 = 'flag-small flag2 ' + get_flag(this.props.user.predictions[2]);
+        var flag3 = 'flag-small flag3 ' + get_flag(this.props.user.predictions[3]);
+        var level_icon = 'http://local.brazil2014.reutersmedia.net:8000/images/small_expertise_' + level[this.props.user.level] + '.png';
         return (
           <tr className="row">
-            <td>
-              <span className="rank">{this.props.rank}</span>
-              <span className="score">{this.props.score}</span>
+            <td className="position">
+              <span className="rank">{this.props.user.rank}</span><br/>
+              <span className="score">{score}</span>
             </td>
-            <td>
-              <img className="profile-pic" src={this.props.profile_picture_url}/>
-              <span className="first-name">{this.props.first_name}</span>
-              <span className="last-name">{this.props.last_name}</span>
+            <td className="name">
+              <img className="profile-pic" width="30" height="30" src={this.props.user.picture_url}/>
+              <div className="fullname">
+                {this.props.user.first}<br/>{this.props.user.last}
+              </div>
             </td>
-            <td>
-              <img className="first-pick" src={this.props.match_1_pick_url}/>
-              <img className="second-pick" src={this.props.match_2_pick_url}/>
-              <img className="third-pick" src={this.props.match_3_pick_url}/>
-              <img className="forth-pick" src={this.props.match_4_pick_url}/>
+            <td className="predictions">
+                <span className={flag0}></span>
+                <span className={flag1}></span>
+                <span className={flag2}></span>
+                <span className={flag3}></span>
             </td>
-            <td>
-              <img className="expert-level" src={this.props.expert_level}/>
+            <td className="expert-level last">
+              <img src={level_icon}/>
             </td>
           </tr>
         );
       }
     }),
 
+    Prediction: React.createClass({
+      render: function() {
+        console.log(this.props.prediction);
+        return (
+          <div className="my-prediction">
+            
+          </div>
+        );
+      }
+    }),
+
+    Top: React.createClass({
+      render: function() {
+        console.log('top');
+        console.log(this.props.my_predictions);
+        return (
+          <div className="top-container">
+            <img src='http://local.brazil2014.reutersmedia.net:8000/images/header.png'/>
+            <div className="top">
+              <div>
+                <img className="my-profile-image" src={this.props.user.picture_url}/>
+                <div className="my-name">{this.props.user.first} {this.props.user.last}</div>
+              </div>
+              <div>
+                <div className="my-predictions">
+                  <div className="predictions-title">Upcoming Matches</div>
+                  <div className="predictions-subtitle">Chooase a winner</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
+    }),
+
     Widget: React.createClass({
       render: function() {
+        var Row = WCup.Components.GoalKeeper.Row;
+        var Top = WCup.Components.GoalKeeper.Top;
+        var top_predictors = this.props.users.map(function (user, index) {
+            user.rank = index + 1;
+            return <Row user={user} key={user.id}/>;
+        }, this);
         return (
-          <table>
-            <thead>
-              <tr>
-                <th>
-                  Ranks &amp; Points
-                </th>
-                <th>
-                  Name
-                </th>
-                <th>
-                  Next Picks
-                </th>
-                <th>
-                  Expert Level
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-            </tbody>
-          </table>
+          <div>
+            <Top user={this.props.user} my_predictions={this.props.my_predictions}/>
+            <table>
+              <thead>
+                <tr>
+                  <th className="rank">
+                    Ranks / Points
+                  </th>
+                  <th className="name">
+                    Name
+                  </th>
+                  <th className="predictions">
+                    Next Picks
+                  </th>
+                  <th className="level last">
+                    Expert Level
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {top_predictors}
+              </tbody>
+            </table>
+          </div>
         );
       }
     })
